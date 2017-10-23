2647ee887de3b65c37ab764836f4896ce628b156
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-10-23 09:33:22 -0600

    Made a Santa icon when I display text

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,8 +1,15 @@
 package chat.controller;
+import chat.view.PopupDisplay;
 public class ChatController 
 {
+	private PopupDisplay popup;
+	public ChatController()
+	{
+		popup = new PopupDisplay();
+	}
 	public void start()
 	{
-		
+		popup.displayText("Ho Ho Ho. Merry Christmas!");
+		popup.getResponse("How are you?");
 	}
 }
--- src/chat/view/PopupDisplay.java
+++ src/chat/view/PopupDisplay.java
@@ -1,17 +1,28 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class PopupDisplay 
 {
+	
+	private ImageIcon icon;
+	private String windowTitle;
+	
+	public PopupDisplay()
+	{
+		icon = new ImageIcon(getClass().getResource("images/Santa.png"));
+		windowTitle = "Santa says";
+	}
+	
 	public void displayText(String message)
 	{
-		JOptionPane.showMessageDialog(null, message);
+		JOptionPane.showMessageDialog(null, message, windowTitle, JOptionPane.INFORMATION_MESSAGE, icon);
 	}
 	public String getResponse(String question)
 	{
-		String response = "";
-		response += JOptionPane.showInputDialog(null, question);
-		return response;
+		String answer = "";
+		answer += JOptionPane.showInputDialog(null, question, windowTitle, JOptionPane.PLAIN_MESSAGE, icon, null, "");
+		return answer;
 	}
 }
