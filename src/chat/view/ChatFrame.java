3753b76498a8d158e2ae00e6afb6684df5f0da5d
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-11-13 09:48:08 -0700

    added code for frame

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,5 +1,26 @@
 package chat.view;
 
-public class ChatFrame {
+import chat.controller.ChatbotController;
+import javax.swing.JFrame;
 
+public class ChatFrame extends JFrame
+{
+
+	private ChatbotController appController;
+	
+	public ChatFrame(ChatbotController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupFrame();
+	}
+	private void setupFrame()
+	{
+		this.setContentPane(firstPanel);
+		this.setTitle("Color Changer");
+		this.setSize(500, 500);
+		this.setResizable(true);
+		this.setVisible(true);
+	}
 }
