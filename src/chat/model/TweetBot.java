7e704efb4c183d6f3ad6e1691491cb8509166af7
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-02-15 09:55:32 -0700

    Added a twitter thing

---
+++ src/chat/model/TweetBot.java
@@ -0,0 +1,19 @@
+package chat.model;
+
+public class TweetBot 
+{
+//	oauth.consumerKey = E6TSAm2ru96IEmb0THrQ1cd3G;
+//	oauth.consumerSecret = y27kXBkxnwSoSUk0ZWgDZ9YEYr0xpCN0VEA681pfQI6M03ONmi;
+//	oauth.accessToken = 	GVoAI5Jw4IhZ3vl9iG9tDSrJrlbHyJk;
+//	oauth.accessTokenSecret = pBsXuqUX2i24IFLOWqwdiEPiE7M1UpYaNn1UlyWTsn6YV;
+	
+	public void sendTweet()
+	{
+		//Don't forget to send @ChatbotCtec
+		
+		
+	}
+	
+	
+	
+}
